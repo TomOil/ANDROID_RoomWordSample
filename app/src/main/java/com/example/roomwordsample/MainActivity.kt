a29d006ba79eebaec4547abec94664a4e9fd539e
t_olejnik <t_olejnik@utb.cz>
2020-12-02 16:36:28 +0100

    Created an Entity

--- app/src/main/java/com/example/roomwordsample/MainActivity.kt
+++ app/src/main/java/com/example/roomwordsample/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.roomwordsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    data class Word(val word: String)
+    {
+        @Entity(tableName = "word_table") //Entity represents a SQLite table with name word_table
+        class Word(
+
+            @PrimaryKey(autoGenerate = true) val id: Int, //primary key with autogenerate option
+
+            @ColumnInfo(name = "word") val word: String // name of the column
+
+        )
     }
 }
